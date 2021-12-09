1d8af988f856eb5465cc4dfe7b1f828ff314e166
cricripaz <49013118+cricripaz@users.noreply.github.com>
2021-12-09 18:27:13 -0400

    PlayerSelect
    Add pantalla seleccionar personaje

--- Assets/Scripts/CambiarPersonaje.cs
+++ Assets/Scripts/CambiarPersonaje.cs
@@ -2,17 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CambiarPersonaje : MonoBehaviour
+public  class CambiarPersonaje : MonoBehaviour
 {
+
+
+
+    private GameObject[] cambiarPersonaje;
+
+    private int index;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cambiarPersonaje = new GameObject[transform.childCount];
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            cambiarPersonaje[i] = transform.GetChild(i).gameObject;
+
+        }
+
+        foreach (GameObject j in cambiarPersonaje)
+        {
+            j.SetActive(false);
+
+
+
+            if (cambiarPersonaje[index])
+            {
+                cambiarPersonaje[index].SetActive(true);
+
+            }
+
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+
         
+    }
+
+    public void btnLeft()
+    {
+        cambiarPersonaje[index].SetActive(false);
+        index--;
+        if(index < 0)
+        {
+            index = cambiarPersonaje.Length - 1 ;
+        }
+
+        cambiarPersonaje[index].SetActive(true);
+
+    }
+
+    public void btnRight()
+    {
+
     }
 }
